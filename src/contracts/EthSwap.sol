f0e90ea71bcc05564c1588b56f1b230a128e4b08
jakepeg <jakepeg@gmail.com>
2021-06-03 11:50:02 +0200

    update

---
+++ src/contracts/EthSwap.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.0;
+
+import "./Token.sol";
+
+contract EthSwap {
+    string public name = "EthSwap Instant Exchange";
+    Token public token;
+    uint256 public rate = 100;
+
+    event TokenPurchased(
+        address account,
+        address token,
+        uint256 amount,
+        uint256 rate
+    );
+
+    constructor(Token _token) public {
+        token = _token;
+    }
+
+    function buyTokens() public payable {
+        uint256 tokenAmount = msg.value * rate;
+        token.transfer(msg.sender, tokenAmount);
+
+        // emit an event
+        emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
+}
